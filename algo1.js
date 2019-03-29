59aa2ddb3f1c0fd3ab431497cd34093533531d08
Lau <laurent.henriques@gmail.com>
2019-03-29 09:27:53 +0100

    Algo1 completed

--- algo1.js
+++ algo1.js
@@ -17,3 +17,20 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+const Count = function() {
+
+  
+  let totals = []
+    for (let i = 1; i <= 26; i++) {
+      for(let b = 1; b <= 100; b++) {
+         totals.push(i + '-' + b) 
+         
+      }
+
+
+    }
+
+
+}
+
