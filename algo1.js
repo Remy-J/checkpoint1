e9effd88214f79578e40d929b5b54ca2948c327e
BLOT Martial <martial.blot@outlook.fr>
2019-03-29 09:48:52 +0100

    algo1 terminé

--- algo1.js
+++ algo1.js
@@ -17,3 +17,22 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+let colonne = 1;
+let siege = 1;
+let nbSiegesParCol = 100;
+let nbSieges = 26*nbSiegesParCol;
+let result = []
+
+function listeSiege (){
+for (let i = 0; i<nbSieges; i++){
+  if (siege > 100){
+    colonne=colonne+1
+    siege=1;
+  }
+  result[i] = `${colonne}-${siege}`
+  siege++
+}
+return result.join("\n")
+}
+console.log (listeSiege ())
